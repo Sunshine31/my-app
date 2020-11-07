3de71d060ecfef85c9de9b06b7d377a64f509a81
Sunshine31 <ocarin31@yandex.ru>
2020-11-07 12:47:15 +0700

    Initial react social network

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://image.shutterstock.com/image-vector/dots-letter-c-logo-design-260nw-551769190.jpg" />
+    </header>
+  );
+};
+
+export default Header;
