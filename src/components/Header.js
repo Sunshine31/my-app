b1265fc1872a635469809d4f2b819470afd45407
Sunshine31 <ocarin31@yandex.ru>
2020-11-07 14:49:31 +0700

    Add css modules.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
+import head from './Header.module.css';
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className={head.header}>
       <img src="https://image.shutterstock.com/image-vector/dots-letter-c-logo-design-260nw-551769190.jpg" />
     </header>
   );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={s.content}>
       <div>
         <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" />
       </div>
@@ -10,9 +11,9 @@ const Profile = () => {
       <div>
         My Posts
         <div>New post</div>
-        <div>
-          <div>Post 1</div>
-          <div>Post 2</div>
+        <div className={s.posts}>
+          <div className={s.item}>Post 1</div>
+          <div className={s.item}>Post 2</div>
         </div>
       </div>
     </div>
